07564ccac4b7bf709f7b9668ba1e965581a1cca7
Monique Wienholds <snuffelien@gmail.com>
2020-11-04 13:43:47 +0100

    wat tekst toegevoegd

--- Eindopdracht.py
+++ Eindopdracht.py
@@ -20,7 +20,12 @@ def disclamer():
     
 
 def Start():
-    print("\nDit is een demo versie, de verhaal stukken zullen zo snel mogelijk worden toegevoegd.")
+    print("\nJe woont in een rustig dorpje maar heb al vaak woorden gehad met een paar mensen die politiek een andere mening hebben. \nNa een dag werken kom je thuis aan alleen om het in vlammen op te zien gaan.")
+    print("Een gerucht gaat het dorp rond dat dit het werk is geweest van de groep mensen onder andere degene waarmee je woorden heb gehad.")
+    print("het schijnt dat ze af willen van iedereen met een andere mening en dit al vaker succesvol hebben gedaan.")
+    print("Nog een ander probleem is dat ze aanhangers door het hele land hebben.")
+    print("\nMaar nu sta jij op hun lijst en moet je er toch levend vanaf zien te komen.")
+    print("\nWat ga je doen? \nA. Je blijft gewoon waar je bent. \nB. Je gaat vluchten.")
     time.sleep(2)
     answer = input("Kies A of B\n")
     if answer.upper() == "A":
@@ -162,7 +167,8 @@ def Storypart13():
         Storypart13()
 
 def Storypart14():
-    print("\nDit is verhaalstuk 14")
+    print("\nJe hebt even geprobeert om te doen of er niks aan de hand is, maar je trekt het niet meer. \nJe besluit om alles achter te laten en het land uit te vluchten.")
+    print("/n")
     time.sleep(2)
     answer = input("Kies A of B\n")
     if answer.upper() == "A":
@@ -224,17 +230,23 @@ def Storypart21():
 
 
 def Ending01():
-    print("\nDit is een neutraal einde")
+    print("\nHet is je gelukt om in Nederland te komen, en na veel moeite om de taal en cultuur te leren.")
+    print("Ben je eindelijk veilig en ingeburgerd en mag je hier blijven.")
     time.sleep(2)
     End()
 
 def Ending02():
-    print("\nDit is een doodlopend einde")
+    print("\nHet is je gelukt om in Nederland te komen, en je doet veel moeite om de taal en cultuur te leren.")
+    print("Je bent in 6 jaar als 8 keer van plek verhuisd en wordt er een beetje gek van.")
+    print("Je bent al jaren bezig om een verblijfs vergunning te krijgen, \nmaar je hebt het gevoel dat je steeds alleen maar van het kastje naar de muur word gestuurd.")
     time.sleep(2)
     End()
 
 def Ending03():
-    print("\nDit is een slecht einde")
+    print("\nNa een paar jaar in Nederland te hebben gewoond merk je dat je thuis gewoon te veel mist.")
+    print("Je hebt de laatste tijd niks meer gehoord over de mensen waarvan je bent gevlucht, \nen maakt de beslissing om terug te gaan.")
+    print("Eenmaal terug lijkt alles in eerste instantie normaal.")
+    print("Maar binnen 3 maanden staan ze weer voor je neus en begint alles van voor af aan.")
     time.sleep(2)
     End()
 
@@ -258,13 +270,14 @@ def End():
 
 
 def Credits():
-    print("\nHeel erg bedankt voor het spel!")
+    print("\nDit is het einde dan, Hier scheiden onze weggen...")
     print("Ik hoop dat je het leuk vond.")
+    time.sleep(2)
     print("\nGemaakt door: Monique Wienholds")
     print("In opdracht van: Rosmerta Goei & Ed Schenk")
     print("Mede mogelijk gemaakt door: Mediacollege Amsterdam")
     print("Met speciale dank aan: Stichting VluchtelingenWerk Nederland")
-    
+    print("\nEn natuurlijk heel veel dank aan jou voor het spellen!")
 
 uitleg()
 
